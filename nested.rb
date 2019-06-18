ce2b4890ab9b94ea79302685af7647c7b1772bb4
Darin Webster <darinleenavy@gmail.com>
2019-06-18 22:07:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -114,9 +114,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
 
-programmer_hash[:alan_kay][:known_for] = "GUI"
-
 programmer_hash[:alans_new_info] = programmer_hash.delete[:known_for]
+
+programmer_hash[:alan_kay][:alans_new_info] = "GUI"
 
 end
 
